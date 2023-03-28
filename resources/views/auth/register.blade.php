6004831b0dc6efc74ee613b7627c9e749223b77c
fabio massa <fabioalessandromassa@gmail.com>
2023-03-28 16:13:44 +0200

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,50 @@
+<x-layout>
+  <x-slot name=title>Registrati</x-slot>
+  <section class="m-0 p-0">
+      <x-navbar/>
+      <x-percorso/>
+
+      <form style="margin-top:10rem" method="POST" action="{{route('register')}}" enctype="multipart/form-data">
+        {{-- enctype permette di gestire file multimediali --}}
+        @csrf
+        <div class="segment text-center mb-5">
+          <h1>Register</h1>
+        </div>
+
+        <section>
+
+          <label>
+            <input name="name" type="text" placeholder="Fullname" class="@error('name') is-invalid @enderror"/>
+          </label>
+          @error('name')
+          <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+
+
+        <label>
+          <input name="email" type="text" placeholder="Email" class="@error('email') is-invalid @enderror"/>
+        </label>
+        @error('email')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <label>
+          <input name="password" type="password" placeholder="Password" />
+        </label>
+        @error('password')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <label>
+          <input name="password_confirmation" type="password" placeholder="Password_confirmation" />
+        </label>
+        @error('password')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
+        <button class="red" type="submit">Registrati</button>
+
+      </form>
+
+  </section>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\PublicController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [PublicController::class, 'home'])->name('home');
+
+// Auth::routes(['verify'=>true]);
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
